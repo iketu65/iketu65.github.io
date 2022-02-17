91ddd12b269d4222bda4d177648d23cdd74518f2
Phakorn Praditthamrong <iketu65@gmail.com>
2022-02-17 13:32:36 +0700

    add intro to js folder

---
+++ intro-to-js/phakorn.js
@@ -0,0 +1,12 @@
+function askMyName() {
+    const name = prompt("Please enter your name", "Enter your name here.");
+ 
+    if (name != null) {
+       document.getElementById("myName").innerHTML = "My name is " + name;
+    }
+ }
+ 
+ function helloWorld() {
+ const h1message = document.getElementById("myName").innerHTML
+    alert(h1message + ' เลิกกับพี่ ได้ดีทุกคน')
+ }
