debd486f3ad18d67d646828372370f99971082e2
Phakorn Praditthamrong <iketu65@gmail.com>
2022-02-18 14:04:08 +0700

    Add calAge()

--- intro-to-js/phakorn.js
+++ intro-to-js/phakorn.js
@@ -1,12 +1,31 @@
 function askMyName() {
-    const name = prompt("Please enter your name", "Enter your name here.");
- 
-    if (name != null) {
-       document.getElementById("myName").innerHTML = "My name is " + name;
-    }
- }
- 
- function helloWorld() {
- const h1message = document.getElementById("myName").innerHTML;
-    alert(h1message + ' "เลิกกับพี่ ได้ดีทุกคน"')
- }
+   const name = prompt("Please enter your name", "Enter your name here.");
+
+   if (name != null) {
+      document.getElementById("myName").innerHTML = "My name is " + name;
+   }
+}
+
+function helloWorld() {
+   const h1message = document.getElementById("myName").innerHTML;
+   alert(h1message + ' "เลิกกับพี่ ได้ดีทุกคน"');
+}
+
+function calAge() {
+   const yearOfBirth = Number(prompt("Please enter your year of birth in A.D."));
+   
+   if (isNaN(yearOfBirth)||(yearOfBirth==0)||(yearOfBirth==1)||(yearOfBirth>2022)) {
+      alert("Please enter your year of birth in A.D");;
+   } else {
+      const age = 2022 - yearOfBirth;
+      alert(`ยินดีด้วย นายอยู่ในประเทศนี้มา ${age} ปีแล้ว`);
+   }
+}
+
+function loadProfile() {
+   const name = prompt("Please enter your name.");
+   const province = prompt("Please enter your province.");
+
+   document.getElementById("my-name").innerHTML = `${name}`;
+   document.getElementById("my-province").innerHTML = `${province}`;
+}
